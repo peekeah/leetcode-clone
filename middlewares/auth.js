96895c9d11d1aa281ed7116baf8784760bc41328
Pranay Kothari <pranay.kothari109@gmail.com>
2023-04-24 10:42:07 +0530

    isAuthenticated middleware addion to verify jwt token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -28,3 +28,26 @@ exports.isAdmin = async(req, res, next) => {
 
     next();  
 }
+
+exports.isAuthenciated = async(req, res, next) => {
+    // token validation
+    const token = req.headers['access-token'];
+    
+    if(!token){
+        const err = new CustomError("token not found", 404);
+        return next(err);
+    }
+    
+    try {
+        // token verification
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.body.tokenData = tokenData;
+        next();
+    } catch (err) {
+        console.log(err)
+        const error = new CustomError(err.message, 401)
+        next(error);
+
+    }
+}
--- routes/submissions.js
+++ routes/submissions.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const submissions = require('../controllers/submissions');
-
+const auth = require('../middlewares/auth')
 
 const router = express.Router();
 
 router.get('/', submissions.getSubmissions);
-router.post('/add', submissions.createSubmission);
+router.post('/add', auth.isAuthenciated, submissions.createSubmission);
 
 
 module.exports = router;
