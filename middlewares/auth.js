968106e19d3aa86c331c9073d018ff79bdba6d4c
Pranay Kothari <pranay.kothari109@gmail.com>
2023-04-25 10:15:36 +0530

    isAdmin fix & authentication and authorization addition to controllers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,10 @@
-const { CustomError } = require("../utils/error");
-const jwt = require("jsonwebtoken");
+const { CustomError } = require('../utils/error');
+const jwt = require('jsonwebtoken');
 
 exports.isAdmin = async(req, res, next) => {
-    const token = req.headers['access-token'];
+    
+    const tokenData = req.body.tokenData;
 
-    // checking if token exists
-    if(!token){
-        const error = new CustomError('Token not found', 400);
-        return next(error);
-    }
-    
-    // token validation
-    let tokenData;
-    try {
-        tokenData = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (err) {
-        console.log(err)
-        const error = new CustomError('Invalid Token', 401);
-        return next(error);
-    }
-    
     // admin role validation
     if(tokenData.role !== 'admin'){
         const error = new CustomError('You are not authorized', 401);
@@ -34,7 +19,7 @@ exports.isAuthenciated = async(req, res, next) => {
     const token = req.headers['access-token'];
     
     if(!token){
-        const err = new CustomError("token not found", 404);
+        const err = new CustomError('token not found', 404);
         return next(err);
     }
     
--- routes/questions.js
+++ routes/questions.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const questions = require('../controllers/questions');
-const { isAdmin } = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 
 const router = express.Router();
 
 router.get('/', questions.getQuestions);
-router.post('/add', isAdmin, questions.addQuestions);
+router.post('/add', [auth.isAuthenciated, auth.isAdmin], questions.addQuestions);
 
 
 module.exports = router;
