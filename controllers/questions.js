28fb448f2b07442a270b600c9058521222b86016
Pranay Kothari <pranay.kothari109@gmail.com>
2023-05-01 22:06:12 +0530

    bulk upload add for questions

--- controllers/questions.js
+++ controllers/questions.js
@@ -4,10 +4,19 @@ const Questions = require("../entity/questions.entity");
 // modify for bulk upload
 exports.addQuestions = async(req, res) => {
     try {
+        let questions;
+        if(!req.body[0]) {
+            questions = [req.body]
+        } else {
+            questions = [...req.body]
+        }
+
         // adding questions in db
         const repo = AppDataSource.getRepository(Questions)
-        await repo.save(req.body)
-        res.status(201).send({ message: 'Question added successfully!' });
+        questions.forEach(question => {
+            repo.save(question)
+        })
+        res.status(201).send({ message: 'Successfully added!' });
         
     } catch (err) {
         console.log(err)
